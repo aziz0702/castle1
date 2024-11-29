23d2988ec3693912fa00c3d9cec4122d43db8711
aziz0702 <azxxe323@gmail.com>
2024-11-30 01:35:44 +0300

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://azxxe323:a900900900@cluster0.nxwcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
